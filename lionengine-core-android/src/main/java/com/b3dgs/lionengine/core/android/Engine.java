b5108435cb1d258ebc679117d6d21d8f91eabc13
DjThunder <contact@b3dgs.com>
2014-11-09 13:08:40 +0100

    Version updated.

--- lionengine-core-android/src/main/java/com/b3dgs/lionengine/core/android/Engine.java
+++ lionengine-core-android/src/main/java/com/b3dgs/lionengine/core/android/Engine.java
@@ -58,7 +58,7 @@ import com.b3dgs.lionengine.core.Verbose;
  * </pre>
  * 
  * @since 13 June 2010
- * @version 7.1.3
+ * @version 7.1.4
  * @author Pierre-Alexandre (contact@b3dgs.com)
  */
 public final class Engine
