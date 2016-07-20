c78f725f4442ce9dd6b4c4d078e661c8e18aaecf
DjThunder <contact@b3dgs.com>
2016-07-20 20:16:53 +0200

    #277: Project import test added. Test utilities added.

---
+++ com.b3dgs.lionengine.editor.test/src/com/b3dgs/lionengine/editor/UtilTests.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2013-2016 Byron 3D Games Studio (www.b3dgs.com) Pierre-Alexandre (contact@b3dgs.com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package com.b3dgs.lionengine.editor;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.eclipse.core.runtime.Platform;
+
+import com.b3dgs.lionengine.Constant;
+import com.b3dgs.lionengine.LionEngineException;
+import com.b3dgs.lionengine.Media;
+import com.b3dgs.lionengine.editor.project.Project;
+import com.b3dgs.lionengine.editor.project.ProjectModel;
+
+/**
+ * Utility related to NL access.
+ */
+public class UtilTests
+{
+    /**
+     * Copy test resource file.
+     * 
+     * @param plugin The plugin name.
+     * @param media The media to copy.
+     * @return The copied file.
+     * @throws LionEngineException If unable to copy file.
+     */
+    public static File copy(String plugin, Media media)
+    {
+        try
+        {
+            final File file = new File(ProjectModel.INSTANCE.getProject().getResourcesPath(), media.getPath());
+            if (!file.exists())
+            {
+                Files.copy(Platform.getBundle(Activator.PLUGIN_ID + plugin + ".test")
+                                   .getResource(ProjectModel.INSTANCE.getProject().getResources()
+                                                + Constant.SLASH
+                                                + media.getPath())
+                                   .openConnection()
+                                   .getInputStream(),
+                           file.toPath());
+            }
+            return file;
+        }
+        catch (final IOException | NullPointerException exception)
+        {
+            throw new LionEngineException(exception, media);
+        }
+    }
+
+    /**
+     * Get media from current project.
+     * 
+     * @param path The media path from resource folder.
+     * @return The media found.
+     */
+    public static Media getMedia(String path)
+    {
+        final Project project = ProjectModel.INSTANCE.getProject();
+        final File file = new File(project.getResourcesPath(), path);
+        return project.getResourceMedia(file);
+    }
+}
