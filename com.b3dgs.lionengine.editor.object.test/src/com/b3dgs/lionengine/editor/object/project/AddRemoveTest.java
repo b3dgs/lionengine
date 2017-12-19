eb035cfb2fdbe1b01fbf12250e7e4fd7c99e974d
DjThunder <contact@b3dgs.com>
2017-12-19 08:22:36 +0100

    Test coverage increased.

--- com.b3dgs.lionengine.editor.object.test/src/com/b3dgs/lionengine/editor/object/project/AddRemoveTest.java
+++ com.b3dgs.lionengine.editor.object.test/src/com/b3dgs/lionengine/editor/object/project/AddRemoveTest.java
@@ -18,10 +18,15 @@
 package com.b3dgs.lionengine.editor.object.project;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicReference;
 
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
+import org.eclipse.swtbot.swt.finder.waits.Conditions;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotCanvas;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,6 +34,8 @@ import org.junit.runner.RunWith;
 import com.b3dgs.lionengine.editor.UtilEditorTests;
 import com.b3dgs.lionengine.editor.object.UtilNl;
 import com.b3dgs.lionengine.editor.project.ImportProjectTest;
+import com.b3dgs.lionengine.editor.utility.UtilPart;
+import com.b3dgs.lionengine.editor.world.view.WorldPart;
 import com.b3dgs.lionengine.game.FeaturableConfig;
 import com.b3dgs.lionengine.util.UtilFolder;
 
@@ -54,7 +61,9 @@ public class AddRemoveTest
            .contextMenu(UtilNl.get("menu.object-add"))
            .click();
 
+        BOT.waitUntil(Conditions.shellIsActive(Messages.Title));
         BOT.activeShell().pressShortcut(Keystrokes.CR);
+
         UtilEditorTests.waitResourcesCopied(BOT, projectFolder, 2);
 
         final File file = new File(projectFolder, UtilFolder.getPath("resources", FeaturableConfig.DEFAULT_FILENAME));
@@ -68,8 +77,65 @@ public class AddRemoveTest
            .contextMenu(UtilNl.get("menu.object-delete"))
            .click();
 
-        UtilEditorTests.waitResourcesCopied(BOT, projectFolder, 2);
+        BOT.sleep(500);
+        BOT.activeShell().pressShortcut(Keystrokes.CR);
+        UtilEditorTests.waitResourcesCopied(BOT, projectFolder, 1);
 
         Assert.assertFalse(file.getPath(), file.isFile());
+    }
+
+    /**
+     * Test the object add on world, move and remove.
+     */
+    @Test
+    public void testObjectWorld()
+    {
+        final File projectFolder = ImportProjectTest.createProject(BOT, getClass());
+
+        BOT.tree(0)
+           .getTreeItem(projectFolder.getName())
+           .getNode("resources")
+           .contextMenu(UtilNl.get("menu.object-add"))
+           .click();
+
+        BOT.waitUntil(Conditions.shellIsActive(Messages.Title));
+        BOT.activeShell().pressShortcut(Keystrokes.CR);
+        UtilEditorTests.waitResourcesCopied(BOT, projectFolder, 2);
+
+        final File file = new File(projectFolder, UtilFolder.getPath("resources", FeaturableConfig.DEFAULT_FILENAME));
+
+        Assert.assertTrue(file.getPath(), file.isFile());
+
+        BOT.tree(0).getTreeItem(projectFolder.getName()).getNode("resources").getNode(file.getName()).click();
+
+        final WorldPart world = UtilPart.getPart(WorldPart.ID, WorldPart.class);
+        final AtomicReference<Rectangle> view = new AtomicReference<>();
+        BOT.activeShell().display.syncExec(() -> view.set(world.getView()));
+
+        final SWTBotCanvas canvas = BOT.canvas(1);
+
+        // Place object clicking inside world
+        canvas.click(view.get().x, view.get().y);
+
+        // Select placed object (bottom left)
+        canvas.click(view.get().x + 10, view.get().y + view.get().height - 10);
+
+        openAndCloseProperty(BOT, com.b3dgs.lionengine.editor.object.properties.clazz.Messages.Class);
+        openAndCloseProperty(BOT, com.b3dgs.lionengine.editor.object.properties.setup.Messages.Setup);
+    }
+
+    /**
+     * Open property dialog and close.
+     * 
+     * @param bot The bot used.
+     * @param property The property key.
+     */
+    private void openAndCloseProperty(SWTBot bot, String property)
+    {
+        bot.tree(1).getTreeItem(property).doubleClick();
+        bot.waitUntil(Conditions.shellIsActive(com.b3dgs.lionengine.editor.dialog.combo.Messages.Title));
+        final SWTBotShell shellProperties = bot.activeShell();
+        shellProperties.bot().button(com.b3dgs.lionengine.editor.dialog.Messages.Finish).click();
+        bot.waitUntil(Conditions.shellCloses(shellProperties));
     }
 }
