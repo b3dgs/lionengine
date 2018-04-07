2d465aeb3d50abb42845cdd8cde1fc67a6b7357c
DjThunder <contact@b3dgs.com>
2018-04-07 17:20:29 +0200

    Sonar fix.

--- lionengine-core-awt/src/test/java/com/b3dgs/lionengine/awt/graphic/GraphicAwtTest.java
+++ lionengine-core-awt/src/test/java/com/b3dgs/lionengine/awt/graphic/GraphicAwtTest.java
@@ -43,7 +43,7 @@ public final class GraphicAwtTest extends GraphicTest
      * Test set graphic.
      */
     @Test
-    public void setGraphic()
+    public void testSetGraphic()
     {
         final GraphicAwt g = new GraphicAwt();
         g.setGraphic(g);
