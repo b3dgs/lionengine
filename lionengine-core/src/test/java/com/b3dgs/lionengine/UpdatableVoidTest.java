64f0e2c53902f73066c75d9fcf5d8f288441675f
DjThunder <contact@b3dgs.com>
2019-05-22 22:41:51 +0200

    Updatable void test added.

---
+++ lionengine-core/src/test/java/com/b3dgs/lionengine/UpdatableVoidTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2013-2018 Byron 3D Games Studio (www.b3dgs.com) Pierre-Alexandre (contact@b3dgs.com)
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package com.b3dgs.lionengine;
+
+import static com.b3dgs.lionengine.UtilAssert.assertNotNull;
+import static com.b3dgs.lionengine.UtilAssert.assertPrivateConstructor;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test {@link UpdatableVoid}.
+ */
+public final class UpdatableVoidTest
+{
+    /**
+     * Test the constructor.
+     */
+    @Test
+    public void testConstructorPrivate()
+    {
+        assertPrivateConstructor(UpdatableVoid.class);
+    }
+
+    /**
+     * Test the get instance.
+     */
+    @Test
+    public void testGetInstance()
+    {
+        assertNotNull(UpdatableVoid.getInstance());
+    }
+}
