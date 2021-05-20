24136c1cd839bc7d1d42569b953b4ab790d304ce
Pierre-Alexandre <contact@b3dgs.com>
2021-05-20 16:53:52 +0200

    #727: SourceResolutionDelegate class added.

---
+++ lionengine-core/src/main/java/com/b3dgs/lionengine/graphic/engine/SourceResolutionDelegate.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2013-2021 Byron 3D Games Studio (www.b3dgs.com) Pierre-Alexandre (contact@b3dgs.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.b3dgs.lionengine.graphic.engine;
+
+import java.util.function.IntSupplier;
+
+/**
+ * Provide resolution from source.
+ */
+public final class SourceResolutionDelegate implements SourceResolutionProvider
+{
+    private final IntSupplier width;
+    private final IntSupplier height;
+    private final IntSupplier rate;
+
+    /**
+     * Create delegate.
+     * 
+     * @param width The width provider.
+     * @param height The height provider.
+     * @param rate The rate provider.
+     */
+    public SourceResolutionDelegate(IntSupplier width, IntSupplier height, IntSupplier rate)
+    {
+        this.width = width;
+        this.height = height;
+        this.rate = rate;
+    }
+
+    @Override
+    public int getWidth()
+    {
+        return width.getAsInt();
+    }
+
+    @Override
+    public int getHeight()
+    {
+        return height.getAsInt();
+    }
+
+    @Override
+    public int getRate()
+    {
+        return rate.getAsInt();
+    }
+}
