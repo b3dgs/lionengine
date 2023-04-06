35656569c84f9d6b924f4adfcb57b59c8cc4487a
Pierre-Alexandre <contact@b3dgs.com>
2023-04-06 19:06:39 +0200

    [#779] Body refactored.

--- lionengine-game/src/main/java/com/b3dgs/lionengine/game/feature/body/BodyConfig.java
+++ lionengine-game/src/main/java/com/b3dgs/lionengine/game/feature/body/BodyConfig.java
@@ -16,11 +16,11 @@
  */
 package com.b3dgs.lionengine.game.feature.body;
 
+import com.b3dgs.lionengine.AttributesReader;
 import com.b3dgs.lionengine.Check;
 import com.b3dgs.lionengine.Constant;
 import com.b3dgs.lionengine.LionEngineException;
 import com.b3dgs.lionengine.Xml;
-import com.b3dgs.lionengine.game.Configurer;
 
 /**
  * Represents the body data.
@@ -46,7 +46,7 @@ public final class BodyConfig
      * @return The config loaded.
      * @throws LionEngineException If unable to read node.
      */
-    public static BodyConfig imports(Configurer configurer)
+    public static BodyConfig imports(AttributesReader configurer)
     {
         Check.notNull(configurer);
 
